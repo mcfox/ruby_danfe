c8d7df97c74db19f7def0e3590a722d8705b4271
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2014-01-30 17:56:35 -0200

    Adicionado a execução da task para recriar os pdfs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,14 @@ SimpleCov.start
 
 require "bundler/setup"
 require "ruby_danfe"
+require 'rake'
 
 Bundler.require(:default, :development)
 Dir[File.dirname(__FILE__) + "/support/*.rb"].each { |f| require f }
+
+load File.expand_path("../../Rakefile", __FILE__)
+Rake::Task["spec:fixtures:recreate_pdfs"].invoke
+# rake spec:fixtures:recreate_pdfs
 
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
